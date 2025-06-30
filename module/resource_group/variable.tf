2d3ca6edbf96fd7fe1497f8bb0584ae9301cb9b9
Vivek6563 <vivek111293@gmail.com>
2025-06-30 23:59:20 +0530

    code added

--- module/resource_group/variable.tf
+++ module/resource_group/variable.tf
@@ -2,4 +2,7 @@ variable "rg_name" {
     description = "The name of the resource group"
     type        = string
 }
-variable "location" {}
+variable "location" {
+    description = "value of the location"   
+    type        = string
+}
